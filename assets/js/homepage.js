10a5547e4836cf4ff77115dcfc9737d5d52b384f
Ashley Smith <ashlynn4567@gmail.com>
2022-03-17 15:29:33 -0500

    added fetch capability to new js file, linked new js file to secondary html file

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,8 +5,7 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 
 
 var formSubmitHandler = function(event) {
-    // prevent the browser from sending the form's 
-    // data to a URL, as we'll handle this ourselves
+    // prevent page from refreshing
     event.preventDefault();
 
     // get value from input element
@@ -14,6 +13,9 @@ var formSubmitHandler = function(event) {
     
     if (username) {
         getUserRepos(username);
+        
+        // clear old content
+        repoContainerEl.textContent = "";
         nameInputEl.value = "";
 
     } else {
@@ -30,13 +32,14 @@ var getUserRepos = function(user) {
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
+            console.log(response);
             response.json().then(function(data) {
                 console.log(data);
                 displayRepos(data, user);
             });
         
         } else {
-            alert("Error: GitHub User Not Found!")
+            alert("Error: " + response.statusText)
         }
     })
     .catch(function(error) {
@@ -46,18 +49,14 @@ var getUserRepos = function(user) {
 
 
 var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
-
-    // clear old content
-    repoContainerEl.textContent = "";
-    repoSearchTerm.textContent = searchTerm;
-
     // check if api returned any repos
     if (repos.length === 0) {
         repoContainerEl.textContent = "No repositories found.";
         return;
     };
+
+    // clear old content
+    repoSearchTerm.textContent = searchTerm;
 
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
@@ -75,25 +74,25 @@ var displayRepos = function(repos, searchTerm) {
         // append to container
         repoEl.appendChild(titleEl);
 
+        //create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        };
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
         // append container to the dom 
         repoContainerEl.appendChild(repoEl);
-    };
-
-    //create a status element
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
-
-    // check if current repo has issues or not
-    if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-    
-    } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    };
-
-    // append to container
-    repoEl.appendChild(statusEl);
+    };    
 };
 
-
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
