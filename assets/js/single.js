aba78708fb7e8e4d02d502d699debd5cb2fbab37
Ashley Smith <ashlynn4567@gmail.com>
2022-03-17 15:41:38 -0500

    converted fetched data into dom elements for github issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,6 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
+
 var getRepoIssues = function(repo) {
     console.log(repo);
 
@@ -7,7 +10,7 @@ var getRepoIssues = function(repo) {
         // request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                displayIssues(data);
             });
         
         } else {
@@ -16,8 +19,43 @@ var getRepoIssues = function(repo) {
     });
 };
 
+
 var displayIssues = function(issues) {
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    };
     
+    for (var i = 0; i < issues.length; i++) {
+        // create a link element to take users to the issue on github
+        var issueEl = document.createElement("a");
+        issueEl.classList = "list-item flex-row justify-space-between align-center";
+        issueEl.setAttribute("href", issues[i].html_url);
+        issueEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+
+        } else {
+            typeEl.textContent = "(Issue)";
+        };
+
+        // append to container
+        issueEl.appendChild(typeEl);
+        issueContainerEl.appendChild(issueEl);
+    };
 };
 
-getRepoIssues("facebook/react");
+
+getRepoIssues("ashlynn4567/Git-It-Done");
